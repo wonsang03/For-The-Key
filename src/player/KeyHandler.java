0b31094f199036571693fb6544b02a16ac91fc06
USER <momok316@mokpo.ac.kr>
2025-12-10 11:06:30 +0900

    Feat: UI 시스템 분리 및 미니맵/게임오버 화면 구현
    [주요 변경 사항]
    1. UIRenderer 클래스 생성 및 UI 로직 분리
       - GamePanel의 UI 그리기 코드를 UIRenderer로 이동
       - 타이틀 화면, 플레이 HUD, 게임오버 화면 상태 관리
    
    2. 신규 기능 구현
       - 미니맵: 우측 상단 맵 구조, 플레이어(초록색 점)/적(빨간색 점) 위치 표시, 문 (노란색)
       - 아이템 슬롯: 좌측 하단 1, 2, 3번 슬롯 UI 추가
       - 게임 오버 연출: 버튼(R) 깜빡임 애니메이션 추가 (P 누르면 게임 오버 화면 뜸, R 누르면 다시 시작)
       - 타이틀 화면: 시작 버튼 깜빡임 애니메이션 추가 (추후 열쇠 이미지 추가하면 좋을 듯)
    
    3. 기타 수정
       - KeyHandler: 1, 2, 3, Q, E 키 입력 상태 추가
       - 리소스: UI 관련 이미지(title, gameover) 추가
       - GamePanel_Minjeong.java: GamePanel 수정 사항 백업 파일 추가 (GamePanel.java에서
    UI 내용 분리 및 미니맵에 적을 표시하기 위해 private ArrayList<enemy.Enemy> enemies = new
    ArrayList<>(); -> public ArrayList<enemy.Enemy> enemies = new
    ArrayList<>(); 으로 변경 필요)

--- src/player/KeyHandler.java
+++ src/player/KeyHandler.java
@@ -5,26 +5,86 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
+    // 플레이어 이동 상태 변수 (Player 클래스에서 사용)
     public boolean upPressed, downPressed, leftPressed, rightPressed;
+    
+    // 키 입력 감지 (나중에 다른 곳에서 쓸 수 있도록 변수는 만들어둠)
+    public boolean onePressed, twoPressed, threePressed;
+    public boolean qPressed, ePressed;
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+        // 사용하지 않음
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
-        if (code == KeyEvent.VK_W) upPressed = true;
-        if (code == KeyEvent.VK_S) downPressed = true;
-        if (code == KeyEvent.VK_A) leftPressed = true;
-        if (code == KeyEvent.VK_D) rightPressed = true;
+
+        // 플레이어 이동 (WASD)
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+        
+        // 아이템 및 기능 키 (GamePanel에서 직접 처리하지만, 상태 저장을 위해 남겨둠)
+        if (code == KeyEvent.VK_1) {
+            onePressed = true;
+        }
+        if (code == KeyEvent.VK_2) {
+            twoPressed = true;
+        }
+        if (code == KeyEvent.VK_3) {
+            threePressed = true;
+        }
+        if (code == KeyEvent.VK_Q) {
+            qPressed = true;
+        }
+        if (code == KeyEvent.VK_E) {
+            ePressed = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
-        if (code == KeyEvent.VK_W) upPressed = false;
-        if (code == KeyEvent.VK_S) downPressed = false;
-        if (code == KeyEvent.VK_A) leftPressed = false;
-        if (code == KeyEvent.VK_D) rightPressed = false;
+
+        // 키를 뗐을 때 상태 false로 변경
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+        
+        if (code == KeyEvent.VK_1) {
+            onePressed = false;
+        }
+        if (code == KeyEvent.VK_2) {
+            twoPressed = false;
+        }
+        if (code == KeyEvent.VK_3) {
+            threePressed = false;
+        }
+        if (code == KeyEvent.VK_Q) {
+            qPressed = false;
+        }
+        if (code == KeyEvent.VK_E) {
+            ePressed = false;
+        }
     }
 }
